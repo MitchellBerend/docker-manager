f4484351b111abc19b8925928af96228e6cf51bf
Mitchell Berendhuysen <45570310+MitchellBerend@users.noreply.github.com>
2022-10-06 17:10:42 +0200

    added tests (#7)

--- src/client/connector.rs
+++ src/client/connector.rs
@@ -1,20 +1,20 @@
+use std::path::Path;
+
 use crate::cli::Command;
 use crate::utility::command;
 
+#[derive(Debug)]
 pub struct Client {
     nodes: Vec<Node>,
 }
 
 impl Client {
-    pub fn from_config() -> Self {
-        let config_path = format!(
-            "{}/.ssh/config",
-            std::env::var("HOME").unwrap_or_else(|_| "/home/root".into())
-        );
+    pub fn from_config<A: AsRef<Path>>(config_path: A) -> Self {
         let file_contents = std::fs::read_to_string(config_path).unwrap_or_else(|_| "".into());
+
         let mut nodes: Vec<Node> = vec![];
         for line in file_contents.split('\n') {
-            if !line.contains('#') && line.starts_with("Host") {
+            if !line.contains('#') && line.starts_with("Hostname") {
                 let s: String = line.replace("  ", "").split(' ').nth(1).unwrap().into();
                 nodes.push(Node::new(s))
             }
@@ -31,6 +31,7 @@ impl Client {
     }
 }
 
+#[derive(Debug)]
 pub struct Node {
     address: String,
 }
@@ -85,6 +86,50 @@ impl std::fmt::Display for NodeError {
     fn fmt(&self, f: &mut std::fmt::Formatter) -> Result<(), std::fmt::Error> {
         match self {
             Self::SessionError(hostname, e) => write!(f, "[NodeError] {}: {}", hostname, e),
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::{Client, Node};
+
+    #[test]
+    fn test_from_config() {
+        let client = Client::from_config("test_files/mock_ssh_config");
+        let correct_nodes: Vec<String> = vec!["abc".into(), "def".into(), "ghi".into()];
+
+        let nodes: Vec<String> = client
+            .nodes
+            .iter()
+            .map(|node| node.address.clone())
+            .collect();
+
+        assert_eq!(correct_nodes, nodes);
+    }
+
+    #[test]
+    fn test_client_info() {
+        let client = Client::from_config("test_files/mock_ssh_config");
+        let correct_nodes: Vec<(String, Node)> = vec![
+            (String::from("abc"), Node::new("abc".into())),
+            (String::from("def"), Node::new("def".into())),
+            (String::from("ghi"), Node::new("ghi".into())),
+        ];
+        let nodes: Vec<(&String, &Node)> = client
+            .nodes_info()
+            .iter()
+            .map(|(hostname, node)| (*hostname, *node))
+            .collect();
+
+        let mut index = 0;
+        for (hostname, node) in nodes {
+            let correct_node = &correct_nodes.get(index).unwrap();
+            let correct_hostname = &correct_node.0;
+            let correct_node_ref = &correct_node.1;
+            assert_eq!(correct_hostname, hostname);
+            assert_eq!(correct_node_ref.address, node.address);
+            index += 1;
         }
     }
 }
